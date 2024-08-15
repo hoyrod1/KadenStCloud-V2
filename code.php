19d5b5e6ccf44a9154230b7ca46c1d9f8b41a6c6
Rodney St Cloud <hoyrod1@aol.com>
2024-08-15 10:42:31 -0400

    Completed the styling for the the code.php file

--- code.php
+++ code.php
@@ -29,38 +29,35 @@ function title()
 require_once "includes/header.php";
 //=============================== ENDING OF HEADER ================================//
 ?>
-<body style="overflow-x: hidden;">
 <!------------------------------- BEGGINING OF BODY -------------------------------->
+<body class="activate-body" style="overflow-x: hidden;">
 <?php
 //========================= BEGGINING OF NAVIGATION BAR ===========================//
 require_once "includes/nav.php";
 //============================ END OF NAVIGATION BAR ==============================//
 ?>
 <!---------------------------- BEGGINING OF MAIN SECTION---------------------------->
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6 m-auto">
-        <div class="card mt-5">
-          <div class="card-title">
-              <h2 class="text-center py-2">Activate</h2>
-          </div>
-          <hr>
-          <?php validateCode(); ?>
-          <div class="card-body">
-            <form method="POST">
-                <input type="text" name="token" placeholder="########" class="form-control mb-2">
-                <div class="card-footer">
-                    <button class=" btn btn-success" name="validate_code">
-                        Activate
-                    </button>
-                    <a href="login.php" class="btn btn-danger float-right">login</a>
-                </div>
-            </form>
-          </div>
-        </div>
-      </div>
+<div class="activate-main-container">
+<hr>
+<?php validateCode(); ?>
+<div class="activate-container">
+  <h1 class="activate-h1">Recover Your Password</h1>
+    <div class="">
+        <form class="activate-form" method="POST">
+            <input type="text" name="token" placeholder="#####" class="activate-inp">
+            <br />
+            <button class="activate-button" name="validate_code">
+                Activate
+            </button>
+            <div class="activate-login">
+                Remember password: &nbsp; 
+                <a href="login.php" class="activate-login-anchor">login</a>
+            </div>
+        </form>
     </div>
-  </div>
+    <hr>
+</div>
+</div>
 <!------------------------------ END OF MAIN SECTION ------------------------------>
 <?php
 //============================= BEGGINING OF FOOTER ===============================//
